8242b11cfd7c963323709d4f6aaf157f3ceae6c7
Gregor von Laszewski <laszewski@gmail.com>
2015-09-12 20:04:16 -0400

    fixing locations to be os independent

--- cloudmesh_base/locations.py
+++ cloudmesh_base/locations.py
@@ -2,7 +2,7 @@ from cloudmesh_base.util import path_expand
 from cloudmesh_base.Shell import Shell
 import os
 
-__config_dir_prefix__ = "~/.cloudmesh"
+__config_dir_prefix__ = os.path.join("~", ".cloudmesh")
 
 __config_dir__ = path_expand(__config_dir_prefix__)
 
@@ -12,7 +12,7 @@ def config_file(filename):
     The location of the config file: ~/.cloudmesh/filename. ~ will be expanded
     :param filename: the filename
     """
-    return __config_dir__ + filename
+    return os.path.join(__config_dir__, filename)
 
 
 def config_file_raw(filename):
@@ -20,7 +20,7 @@ def config_file_raw(filename):
     The location of the config file: ~/.cloudmesh/filename. ~ will NOT be expanded
     :param filename: the filename
     """
-    return __config_dir_prefix__ + filename
+    return os.path.join(__config_dir_prefix__, filename)
 
 
 def config_file_prefix():
