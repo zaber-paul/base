ad10399e21296021e405f2ce7f205b3255959543
Gregor von Laszewski <laszewski@gmail.com>
2015-09-30 08:38:51 -0400

    chg: dev: pep8, minor fix in requirements.txt

--- cloudmesh_base/setup.py
+++ cloudmesh_base/setup.py
@@ -5,20 +5,24 @@ import os
 import pip
 import sys
 
+
 def parse_requirements(filename):
     """ load requirements from a pip requirements file """
     lineiter = (line.strip() for line in open(filename))
     return [line for line in lineiter if line and not line.startswith("#")]
 
+
 def os_execute(commands):
     for command in commands.split("\n"):
         command = command.strip()
-        print (command)
+        print(command)
         os.system(command)
+
 
 def get_version_from_git():
     r = Shell.git('tag').split("\n")[-1]
     return r
+
 
 def check_pip():
     major = int(pip.__version__.split(".")[0])
